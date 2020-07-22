a2f4eae82db95bd1f770210ee8e9b9e6979a5b5e
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-07-22 18:24:33 -0300

    feat(register-order): finished feature register order.

--- core/managers/forms.py
+++ core/managers/forms.py
@@ -1,7 +1,20 @@
 from django.forms import ModelForm
+
 from .models import Order
 
+
 class OrderPostForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['type_order'].widget.attrs.update(
+            {'class': 'form-control'})
+        self.fields['value'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update(
+            {'class': 'form-control'})
+        self.fields['user'].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ('name', 'user', 'type_order', 'value', 'description')
