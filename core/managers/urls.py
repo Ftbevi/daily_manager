d40dfd7b34db5e3b43c9d6ebde72cfdf92641ad4
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-07-22 18:27:10 -0300

    feat(list-orders): wip - list orders in progress.

--- core/managers/urls.py
+++ core/managers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegisterOrderView
+from .views import IndexOrders, RegisterOrderView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('register', RegisterOrderView.as_view(), name='order_register'),
+    path('', IndexOrders.as_view(), name='list_register'),
 ]
--- core/managers/views.py
+++ core/managers/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import ListView
 
 from .forms import OrderPostForm
 from .models import Order
+
+
+class IndexOrders(ListView):
+    model = Order
+    template_name = 'managers/order/list.html'
 
 
 class RegisterOrderView(View):
@@ -12,9 +18,10 @@ class RegisterOrderView(View):
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial={})
         return render(request, self.template_name, {'form': form})
-    
+
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            data = form.save()
-        return render(request, self.template_name, {'data': data})
+            form.save()
+            return redirect('/orders')
+        return render(request, self.template_name, {'form': form})
