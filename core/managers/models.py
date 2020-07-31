0f6d046674be04b8f7af59688c76d277f5826dbe
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2020-07-31 00:51:13 -0300

    changed order for description optional and texts in TYPE_ORDE.

--- core/managers/models.py
+++ core/managers/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Order(models.Model):
     TYPE_ORDES = [
-        ('EXPENSE', 'Expense'),
-        ('REVENUE', 'Revenue'),
+        ('expense', 'expense'),
+        ('revenue', 'expense'),
     ]
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     value = models.DecimalField(max_digits=5, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     type_order = models.CharField(max_length=7, choices=TYPE_ORDES, default='EXPENSE')
